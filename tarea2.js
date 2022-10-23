83f1d3fae9b1e78836870f41be022fc6af8599bc
quirama1120 <quirama1120@gmail.com>
2022-10-22 20:10:57 -0500

    segunda tarea de js en openbootcamp

---
+++ tarea2.js
@@ -0,0 +1,9 @@
+const lista = [
+"christian", 27, true, "20 de nov del 1994", libro = 
+{titulo: "crimen y castigo",
+autor: "Dostoievski",
+fecha: "1867",
+link: "https://www.amazon.com/-/es/Fi%C3%B3dor-Dostoievski-ebook/dp/B00HVZM3F2",
+}
+]
+console.log(lista)
