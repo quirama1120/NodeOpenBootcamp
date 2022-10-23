c010cf321d9748c9d1054eef0d72da54da682122
quirama1120 <quirama1120@gmail.com>
2022-10-23 01:46:01 -0500

    subiendo la tarea 3 de js y controles en openbootcamp

---
+++ tarea3.js
@@ -0,0 +1,21 @@
+//factorial de 10 con for loop
+for (let i = 1; i <= 10; i++) {
+    const resultado = i 
+    console.log(resultado)
+}
+//factorial de 10 con while
+let i = 0;
+while (i<=10) {
+     let resultadoWhile = i;
+    i++;
+    console.log(resultadoWhile)
+}
+//factorial de 10 con bifurcaión de if y sentencia break
+i=0;
+while (i <= 10) {
+    let bi = i;
+    i++;
+    if (i === 7)
+    break;
+    console.log(bi)
+}
