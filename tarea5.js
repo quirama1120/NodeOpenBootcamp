17947caae9655bdb157f85bf40b831d0b17cb9aa
quirama1120 <quirama1120@gmail.com>
2022-10-26 00:58:39 -0500

    subiendo tarea 5 de js con openbootcamp

---
+++ tarea5.js
@@ -0,0 +1,15 @@
+const altura = 178;
+const alturaM = 1.78;
+const peso = 98.7;
+const alturaRedondeada = Math.ceil(alturaM)
+console.log(alturaRedondeada)
+const pesoRedondeado = Math.floor(peso)
+console.log(pesoRedondeado)
+const max1 = Number.MAX_VALUE + 1;
+const max = Number.MAX_VALUE;
+console.log(max1, max)
+if (max1 === max) {
+console.log("son iguales")
+} else {
+    console.log("son diferentes")
+}
