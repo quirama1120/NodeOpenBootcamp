f914efcc8868dc9df745e7e5ef44e49be6e18b36
quirama1120 <quirama1120@gmail.com>
2022-10-27 01:28:15 -0500

    subiendo tarea 7 con js en openbootcamp

---
+++ tarea7.js
@@ -0,0 +1,7 @@
+const family = new Set(["madre","hermano","christian"]);
+
+console.log(family)
+const familyAdd = family.add("christian")
+console.log(familyAdd)
+const familyJs = family.add("Javascript")
+console.log(familyJs)
